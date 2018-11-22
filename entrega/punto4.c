1c653fcf4efe83e8138173a156e647d1b9d7425a
meneraing <ginoipoliti7@gmail.com>
2018-11-22 20:20:56 -0300

    .

--- entrega/punto4.c
+++ entrega/punto4.c
@@ -5,11 +5,10 @@
 void salida();
 
 void main(){
-wiringPiSetupGpio (void) ;
+	wiringPiSetupGpio() ;
 	int a=0, i=0;
 	int ins[]={17,5,6,13,19};
 	int outs[]={23,24,25,12};
-	wiringPiSetup();
 	for(i=0;i<5;i++)
 		pinMode(ins[i], INPUT);
 	for(i=0;i<4;i++)
