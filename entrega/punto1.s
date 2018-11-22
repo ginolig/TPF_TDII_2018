f6145203fea7bd313fcbccf9dc12268259caf038
meneraing <ginoipoliti7@gmail.com>
2018-11-21 21:30:29 -0300

    readme update

--- entrega/punto1.s
+++ entrega/punto1.s
@@ -2,11 +2,24 @@
 .arm
 .global menu
 menu:
-        mov   r1, r0
-loop:   sub   r1, #1
-        cmp   r1, #0
-        bhi   loop
+        cmp   r0, #1    @case 1
+        beq   op1
+        cmp   r0, #2    @case 2
+        beq   op2
+        cmp   r0, #3    @case 3
+        beq   op3
+        cmp   r0, #4    @case 4
+        beq   op4
+        mov   r0, #0    @default
         mov   pc, lr
 
+op1:    mov r0, #1
+        mov pc, lr
+op2:    mov r0, #2
+        mov pc, lr
+op3:    mov r0, #3
+        mov pc, lr
+op4:    mov r0, #4
+        mov pc, lr
 @constantes
 .end
