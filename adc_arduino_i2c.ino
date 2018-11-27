df08baf6910b9320f28015088fac213dccc52226
Gino Ipóliti <38791375+meneraing@users.noreply.github.com>
2018-11-27 11:19:41 -0300

    Update adc_arduino_i2c.ino

--- adc_arduino_i2c.ino
+++ adc_arduino_i2c.ino
@@ -6,8 +6,8 @@ void setup(){
   Wire.begin(0x22); //inicia conexión i2c en la dirección #0x22
   Wire.onRequest(lectura_adc);
   Wire.onReceive(selec_adc);
-  pinMode(A5, INPUT);
-  pinMode(A4, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
   pinMode(A3, INPUT);
   pinMode(A2, INPUT);
 }
@@ -25,17 +25,17 @@ void selec_adc(int bytesReceived){
   while(Wire.available()){
     canal=Wire.read();
     switch(canal){
-      case 5:
-      val_8bits=(analogRead(A5)/4);
+      case 0:
+      val_8bits=(analogRead(A0)/4);
       break;
-      case 4:
-      val_8bits=(analogRead(A4)/4);
-      break;
-      case 3:
-      val_8bits=(analogRead(A3)/4);
+      case 1:
+      val_8bits=(analogRead(A1)/4);
       break;
       case 2:
       val_8bits=(analogRead(A2)/4);
+      break;
+      case 3:
+      val_8bits=(analogRead(A3)/4);
       break;
       default:
       break;
