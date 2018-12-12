59f0c5d448f5288b29ac1e4d34e299f7ce6a0685
meneraing <ginoipoliti7@gmail.com>
2018-12-12 13:11:13 -0300

    commit estetico

--- internal.c
+++ internal.c
@@ -11,48 +11,58 @@ extern void sec2();
 
 void main() {
 int op, tiempo[]={200,300,500,750,100,100,100,100};
-	system("clear");
 
-	printf("Ingrese el modo que desea iniciar:");
-	printf("\n\t 1- El auto fantastico ");
-	printf("\n\t 2- La carrera");
-	printf("\n\t 3- La apilada");
-	printf("\n\t 4- El choque");
-	printf("\n\t 5- Personalizado 1");
-	printf("\n\t 6- Personalizado 2");
-	printf("\n\t 7- Personalizado 3");
-	printf("\n\t 8- Personalizado 4");
-	printf("\n\t 9- Seteo de velocidades mediante el uso de potenciometro");
-	printf("\n\t 10- Posicion de los switches de entrada \n\t ");
+	do{
+		system("clear");
 
-scanf("%d", &op);
+		printf("Ingrese el modo que desea iniciar:");
+		printf("\n\t 1- El auto fantastico ");
+		printf("\n\t 2- La carrera");
+		printf("\n\t 3- La apilada");
+		printf("\n\t 4- El choque");
+		printf("\n\t 5- Personalizado 1");
+		printf("\n\t 6- Personalizado 2");
+		printf("\n\t 7- Personalizado 3");
+		printf("\n\t 8- Personalizado 4");
+		printf("\n\t 9- Seteo de velocidades mediante el uso de potenciometro");
+		printf("\n\t 10- Salir. \n\t ");
 
-	switch(op)
+		scanf("%d", &op);
 
-		case 1:
-		fantastic(tiempo[op-1]);
-		break;
-		case 2:
-		carrera(tiempo[op-1]);
-		break;
-		case 3:
-		apilada(tiempo[op-1]);
-		break;
-		case 4:
-		choque(tiempo[op-1]);
-		break;
-		case 5:
-		sec1();
-		break;
-		case 6:
-		sec2();
-		break;
-		case 7:
-		case 8:
-		case 9:
-		adc(tiempo);
-		break;
-		case 10:
-		default:
+		switch(op){
 
+			case 1:
+			printf("<<< El auto fantastico >>>\n");
+			fantastic(tiempo[op-1]);
+			break;
+			case 2:
+			printf("<<< La carrera >>>\n");
+			carrera(tiempo[op-1]);
+			break;
+			case 3:
+			printf("<<< La apilada >>>\n");
+			apilada(tiempo[op-1]);
+			break;
+			case 4:
+			printf("<<< El choque >>>\n");
+			choque(tiempo[op-1]);
+			break;
+			case 5:
+			sec1();
+			break;
+			case 6:
+			sec2();
+			break;
+			case 7:
+			case 8:
+			case 9:
+			adc(tiempo);
+			break;
+			case 10:
+			break;
+			default:
+			printf("Opcion invalida!\n");
+			break;
+		}
+	}while(op=!10);
 }
