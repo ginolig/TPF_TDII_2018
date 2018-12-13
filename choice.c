93313216d8b141384f3c4a2037f35368109b98a2
altagracience <34692172+altagracience@users.noreply.github.com>
2018-12-13 16:02:27 -0300

    Update choice.c

--- choice.c
+++ choice.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int choice(){
+int main(){
 
-int a, flag=0;
+char a;
 
 system("clear"); 	
-	
 printf("Seleccione la procedencia de los datos\n 1- Perifericos locales \n 2- Comunicacion serie \n");
 
-while(flag==0){
-	scanf("%d", &a);
-	if (a==1)	return 0;
-	else if (a==2)	return 1;
+while(1){
+	scanf("%c", &a);
+	if (a=='1')	return 0;
+	else if (a=='2')	return 1;
+	else if (a == '\n'); //en el buffer queda un enter que ensuciaba la leida
 	else printf("\n El numero ingresado es invalido, intente nuevamente\n");
+
 	
 }
 
