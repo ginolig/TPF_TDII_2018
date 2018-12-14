4ba0b9c841ea64271823a98703b3aa8685135584
altagracience <34692172+altagracience@users.noreply.github.com>
2018-12-14 19:42:41 -0300

    andando

--- carrera.c
+++ carrera.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
-extern int adc();
 
-void carrera(int t){
-  initscr();
+extern int adc();
+extern int kbhit();
+
+void main(int t){
+
   wiringPiSetupGpio() ;
   char c;
-  int i=0, j=0, retardo;
+  int i=0, j=0, retardo, flag=0;
   int lut[16][8]={
       {1,0,0,0,0,0,0,0},
       {1,0,0,0,0,0,0,0},
@@ -31,7 +33,7 @@ void carrera(int t){
   printf("USTED ESTA HACIENDO USO DE LA MAGIA CARRERISTICA\n");
   printf("Pulse el maravilloso botón de la plaqueta para salir\n");
 
-  retardo = adc() * 8;
+  retardo = adc();
   pinMode(17, INPUT);
   for(i=0;i<8;i++)
     pinMode(pins_leds[i], OUTPUT);
@@ -42,20 +44,32 @@ void carrera(int t){
         digitalWrite(pins_leds[j], lut[i][j]);
         if (digitalRead(17) == 1) break;
       
-        for (j = 1; i < retardo; ++i){ //hago el retardo dividido retardo por si aprieto para apagar cuando este esta sucediendo
-            delay(retardo/retardo);
-            if (digitalRead(17) == 1) flag=1; 
-            noecho();
-            flushinp();
-            if(c = getch() == '\033')   if( c = getch() == '[')      if( c = getch() == 'A') retardo+=50; //modo de observar si se pulso flecha arriba
-            if(c = getch() == '\033')   if( c = getch() == '[')      if( c = getch() == 'B') retardo+=50; //modo de observar si se pulso flecha abajo
-            echo(); 
-
-        } 
+        for (j = -1; j < retardo; ++j) //bucle de retardos 1 para poder salir cuando quiera
+	        {
+	            delay(1);
+	            if (digitalRead(17) == 1) { flag=1; break;} 
+	         
+		
+		if(kbhit()){ //detecto si pulso una tecliña
+	        system("/bin/stty raw");   //el buffer se vacia sin esperar intro
+			if( c = getchar() == '[')      
+				if( c = getchar() == 'A'){ //modo de observar si se pulso flecha abajo //detecto si es flecha arriba y disminuyo retardo
+					if(retardo != 0) retardo-=10; 
+					j=-1; 
+					system("clear"); 
+					printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
+				else  { 		//detecto si es flecha abajo y aumento retardo
+					retardo+=10; 
+					j=-1; 
+					system("clear"); 
+					printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
+	        system("/bin/stty cooked"); //pongo el buffer como corresponde
+		}
+        	}
 	   if (digitalRead(17) == 1) break;
 	   }
 
     }
-   for(i=0;i<8;i++) digitalWrite(pins_leds[i], 0);
-    endwin();
+    for(i=0;i<8;i++)  digitalWrite(pins_leds[i], 0);
+    system("clear");
 }
