d3c7a715077b1c3e8a23402a2931b308505527be
altagracience <34692172+altagracience@users.noreply.github.com>
2018-12-14 19:20:12 -0300

    andando

--- apilada.c
+++ apilada.c
@@ -1,54 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
-#include <curses.h>
+//#include <curses.h>
+
 extern int adc();
+extern int kbhit();
 
-void apilada(){
-    initscr();
-    wiringPiSetupGpio();
-    char c;
-    int i=0, max=8, retardo;
-    int pins_leds[]={23,24,25,12,16,20,21,26};
-    pinMode(17, INPUT);
-    for(i=0;i<8;i++)
-      pinMode(pins_leds[i], OUTPUT);
+int main(){
 
-    retardo = adc() * 8;
+	wiringPiSetupGpio();
+	char c;
+	int  retardo, j;
+	int i=0, k, flag=0, mx=8;
+	int pins_leds[]={23,24,25,12,16,20,21,26};
+	pinMode(17, INPUT);
+	for(i=0;i<8;i++) pinMode(pins_leds[i], OUTPUT);
+
+	retardo = adc() ; //llamo al adc para ver su valo, si falla la comunicacion 125
         system("clear");
-    printf("USTED ESTA HACIENDO USO DE LA INIGUALABLE APILADA\n");
-    printf("Pulse el maravilloso botón de la plaqueta para salir\n");
+	printf("USTED ESTA HACIENDO USO DE LA INIGUALABLE APILADA\n");
+	printf("Pulse el maravilloso botón de la plaqueta para salir\n");
+	i=0;
+	while(1){
+	      
+		for (i = 0; i < 8; i++){
+			for(k=0;k<mx;k++){
+						
+				        digitalWrite(pins_leds[k], 1);
 
-    while(digitalRead(17) != 1){
-      
-      for (i = 0; i < max; i++){
-        digitalWrite(pins_leds[i], 1);
-        digitalWrite(pins_leds[i], 0);
-        
-        if (digitalRead(17) == 1) break;
-      
-        for (j = 1; i < retardo; ++i){ //hago el retardo dividido retardo por si aprieto para apagar cuando este esta sucediendo
-            delay(retardo/retardo);
-            if (digitalRead(17) == 1) flag=1; 
-            noecho();
-            flushinp();
-            if(c = getch() == '\033')   if( c = getch() == '[')      if( c = getch() == 'A') retardo+=50; //modo de observar si se pulso flecha arriba
-            if(c = getch() == '\033')   if( c = getch() == '[')      if( c = getch() == 'B') retardo+=50; //modo de observar si se pulso flecha abajo
-            echo(); 
+					for (j = -1; j < retardo; ++j) //bucle de retardos 1 para poder salir cuando quiera
+				        {
+				            delay(1);
+				            if (digitalRead(17) == 1) { flag=1; break;} 
+				         
+					
+						if(kbhit()){ //detecto si pulso una tecliña
+					        system("/bin/stty raw");   //el buffer se vacia sin esperar intro
+							if( c = getchar() == '[')      
+								if( c = getchar() == 'A'){ //modo de observar si se pulso flecha abajo //detecto si es flecha arriba y disminuyo retardo
+									if(retardo != 0) retardo-=10; 
+									j=-1; 
+									system("clear"); 
+									printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
+								else  { 		//detecto si es flecha abajo y aumento retardo
+									retardo+=10; 
+									j=-1; 
+									system("clear"); 
+									printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
+					        system("/bin/stty cooked"); //pongo el buffer como corresponde
+						}
+			        	}
+			
+				        digitalWrite(pins_leds[k], 0);
+	        	}
 
-        }    
-
-        if(i==(max-1)){
-          digitalWrite(pins_leds[i], 1);
-          digitalWrite(pins_leds[i], 0);
-          digitalWrite(pins_leds[i], 1);
-          if (digitalRead(17) == 1) break;
-        }
-
-        if (digitalRead(17) == 1) break;
-      }
-    }
+		mx=mx-1;
+		digitalWrite(pins_leds[mx], 1);
+		
+	
+	        if (digitalRead(17) == 1 || flag == 1) break;
+		}
+	
+	for(i=0;i<8;i++) digitalWrite(pins_leds[i], 0);
+	i=0;
+	k=0;
+	mx=8;
+	if (digitalRead(17) == 1 || flag == 1) break;
+	}
     
     for(i=0;i<8;i++) digitalWrite(pins_leds[i], 0);
-    endwin();
+    system("clear");
+
 }
