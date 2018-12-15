8eeeabfa639da4ed91f8a4e94de9ab7f8a6105bd
Gino Ipóliti <38791375+meneraing@users.noreply.github.com>
2018-12-15 13:31:01 -0300

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,13 @@
+extern int adc();
+extern int ingreso();
+extern int choice();
+extern int internal();
+extern int external();
+extern int fantastic();
+extern int carrera();
+extern int apilada();
+extern int choque();
+extern int kbhit();
+extern int sec1();
+extern int sec2();
+extern int yuta();
