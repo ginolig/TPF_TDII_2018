14c93f61119993dd024dda05b2c93b795af18320
altagracience <34692172+altagracience@users.noreply.github.com>
2018-12-15 15:42:53 -0300

    Update carrera.c

--- carrera.c
+++ carrera.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
-#include "funciones.h"
 
-void carrera(){
+extern int adc();
+extern int kbhit();
+
+void carrera(int t){
 
   wiringPiSetupGpio() ;
   char c;
@@ -28,10 +30,12 @@ void carrera(){
   };
   int pins_leds[]={23,24,25,12,16,20,21,26};
 
+  retardo = adc();
+
   printf("USTED ESTA HACIENDO USO DE LA MAGIA CARRERISTICA\n");
   printf("Pulse el maravilloso botón de la plaqueta para salir\n");
 
-  retardo = adc();
+
   pinMode(17, INPUT);
   for(i=0;i<8;i++)
     pinMode(pins_leds[i], OUTPUT);
@@ -41,29 +45,28 @@ void carrera(){
       for(j=0;j<8;j++)
         digitalWrite(pins_leds[j], lut[i][j]);
         if (digitalRead(17) == 1) break;
-      
-        for (j = -1; j < retardo; ++j) //bucle de retardos 1 para poder salir cuando quiera
+	      for (j = -1; j < retardo; ++j) //hago el retardo dividido retardo por si aprieto para apagar cuando este esta sucediendo
 	        {
 	            delay(1);
 	            if (digitalRead(17) == 1) { flag=1; break;} 
 	         
 		
-		if(kbhit()){ //detecto si pulso una tecliña
-	        system("/bin/stty raw");   //el buffer se vacia sin esperar intro
-			if( c = getchar() == '[')      
-				if( c = getchar() == 'A'){ //modo de observar si se pulso flecha abajo //detecto si es flecha arriba y disminuyo retardo
+		if(kbhit()){
+	        system("/bin/stty raw");   
+			if( c = getchar() == '[')      c = getchar();
+				if( c  == 'A'){ //modo de observar si se pulso flecha abajo
 					if(retardo != 0) retardo-=10; 
 					j=-1; 
 					system("clear"); 
 					printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
-				else  { 		//detecto si es flecha abajo y aumento retardo
+				else if ( c == 'B') { //flecha arriba
 					retardo+=10; 
 					j=-1; 
 					system("clear"); 
 					printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
-	        system("/bin/stty cooked"); //pongo el buffer como corresponde
+	        system("/bin/stty cooked");
+			}
 		}
-        	}
 	   if (digitalRead(17) == 1) break;
 	   }
 
