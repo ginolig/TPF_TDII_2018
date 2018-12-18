6f8c57a928c641fecab8e0d7cb311d6aca52f38f
altagracience <34692172+altagracience@users.noreply.github.com>
2018-12-18 00:36:20 -0300

    Update carrera.c

--- carrera.c
+++ carrera.c
@@ -3,7 +3,7 @@
 #include <wiringPi.h>
 #include "funciones.h"
 
-void carrera(){
+int carrera(){
 
   wiringPiSetupGpio() ;
   char c;
@@ -29,10 +29,10 @@ void carrera(){
   int pins_leds[]={23,24,25,12,16,20,21,26};
 
   retardo = adc();
+  system("clear");
 
   printf("USTED ESTA HACIENDO USO DE LA MAGIA CARRERISTICA\n");
-  printf("Pulse el maravilloso botón de la plaqueta para salir\n");
-
+  printf("Pulse el distinguido botón de la plaqueta para salir\n (PUEDE MODIFICAR LAS VELOCIDADES CON LAS FLECHAS UP AND DOWN)");
 
   pinMode(17, INPUT);
   for(i=0;i<8;i++)
@@ -42,11 +42,11 @@ void carrera(){
     for (i = 0; i < 16; i++){
       for(j=0;j<8;j++)
         digitalWrite(pins_leds[j], lut[i][j]);
-        if (digitalRead(17) == 1) break;
+        if (digitalRead(17) == 1) return 0;
 	      for (j = -1; j < retardo; ++j) //hago el retardo dividido retardo por si aprieto para apagar cuando este esta sucediendo
 	        {
 	            delay(1);
-	            if (digitalRead(17) == 1) { flag=1; break;} 
+	            if (digitalRead(17) == 1) { flag=1; return 0;} 
 	         
 		
 		if(kbhit()){
@@ -56,16 +56,16 @@ void carrera(){
 					if(retardo != 0) retardo-=10; 
 					j=-1; 
 					system("clear"); 
-					printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
+					printf("Pulse el distinguido botón de la plaqueta para salir\n (PUEDE MODIFICAR LAS VELOCIDADES CON LAS FLECHAS UP AND DOWN)");} 
 				else if ( c == 'B') { //flecha arriba
 					retardo+=10; 
 					j=-1; 
 					system("clear"); 
-					printf("Pulse el maravilloso botón de la plaqueta para salir\n");} 
+					printf("Pulse el distinguido botón de la plaqueta para salir\n (PUEDE MODIFICAR LAS VELOCIDADES CON LAS FLECHAS UP AND DOWN)");} 
 	        system("/bin/stty cooked");
 			}
 		}
-	   if (digitalRead(17) == 1) break;
+	   if (digitalRead(17) == 1) return 0;
 	   }
 
     }
