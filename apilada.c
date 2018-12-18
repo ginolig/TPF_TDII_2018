d7bb80115b5820f547e0ac36f557df27427730a5
altagracience <34692172+altagracience@users.noreply.github.com>
2018-12-18 00:34:38 -0300

    Update apilada.c

--- apilada.c
+++ apilada.c
@@ -3,7 +3,7 @@
 #include <wiringPi.h>
 #include "funciones.h"
 
-void apilada(){
+int apilada(){
 
 	wiringPiSetupGpio();
 	char c;
@@ -16,7 +16,7 @@ void apilada(){
 	retardo = adc() ; //llamo al adc para ver su valo, si falla la comunicacion 125
         system("clear");
 	printf("USTED ESTA HACIENDO USO DE LA INIGUALABLE APILADA\n");
-	printf("Pulse el maravilloso botón de la plaqueta para salir\n");
+	printf("Pulse el distinguido botón de la plaqueta para salir\n (PUEDE MODIFICAR LAS VELOCIDADES CON LAS FLECHAS UP AND DOWN)");
 	i=0;
 	while(1){
 	      
@@ -27,7 +27,7 @@ void apilada(){
 				        for (j = -1; j < retardo; ++j) //hago el retardo dividido retardo por si aprieto para apagar cuando este esta sucediendo
 				        {
 				            delay(1);
-				            if (digitalRead(17) == 1) { flag=1; break;} 
+				            if (digitalRead(17) == 1) { flag=1; return 0;} 
 				         
 					
 					if(kbhit()){
@@ -54,14 +54,14 @@ void apilada(){
 		digitalWrite(pins_leds[mx], 1);
 		
 	
-	        if (digitalRead(17) == 1 || flag == 1) break;
+	        if (digitalRead(17) == 1 || flag == 1) return 0;
 		}
 	
 	for(i=0;i<8;i++) digitalWrite(pins_leds[i], 0);
 	i=0;
 	k=0;
 	mx=8;
-	if (digitalRead(17) == 1 || flag == 1) break;
+	if (digitalRead(17) == 1 || flag == 1) return 0;
 	}
     
     for(i=0;i<8;i++) digitalWrite(pins_leds[i], 0);
