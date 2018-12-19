dcd8ef46c14ebf2bd5e5b3bd299544c125deed76
Gino Ipóliti <38791375+meneraing@users.noreply.github.com>
2018-12-19 17:16:14 -0300

    Update sec1.c

--- Final_siendo_void/sec1.c
+++ Final_siendo_void/sec1.c
@@ -6,7 +6,7 @@
 void sec1(){
     wiringPiSetupGpio() ;
     int i=0;
-    int pins_leds[]={23,24,25,12,16,20,21,26};
+    int pins_leds[]={23,24,25,12,16,20,21,26}, estado[2][8]={1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1};
     pinMode(17, INPUT);
     for(i=0;i<8;i++)
       pinMode(pins_leds[i], OUTPUT);
@@ -16,7 +16,7 @@ void sec1(){
 
     while(digitalRead(17) != 1){
       for(i = 0; i < 8; i++){
-        digitalWrite(pins_leds[i], 1);
+        digitalWrite(pins_leds[i], estado[0][i]);
         if (digitalRead(17) == 1) break;
       }
       for (int i = 0; i < (rand()%1000)+1; ++i){ /*Genera numero aleatorio entre 1 y 1000*/
@@ -25,7 +25,7 @@ void sec1(){
       	}
          
       for(i = 0; i < 8; i++){
-        digitalWrite(pins_leds[i], 0);
+        digitalWrite(pins_leds[i], estado[1][i]);
         if (digitalRead(17) == 1) break;
       }
       delay(rand() % 1000 + 1);
